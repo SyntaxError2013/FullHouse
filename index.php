46445f99487b45675e765bbad90bcca23557ec05
Aditya <batu.aditya007@gmail.com>
2013-10-20 10:05:01 +0530

    homepage frontend - finishing required

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <html>
 <head>
-	<title></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Hack-App</title>
+    <script type="text/javascript" src="js/TweenLite.min.js"></script>
+    <script type="text/javascript" src="js/TweenMax.min.js"></script>
+    <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    
 </head>
 <body>
+    <?php include "includes/header.php" ?>
     
-    <form action="analysis.php" method="GET">
-		<div> <input type="text" placeholder="Enter name" name="query" /> </div>
-		<div> <input type="submit" value="GO" /> </div>
-    </form>
+     <div class="body">
+    	<div class="clear"></div>
+    	<div class="body_content">
+        	
+            <div class="buttons">
+            	<div><h2>Enter the name of the politician you want to know about</h2></div>  
+                <div id="form-place">
+                	<form id="form-app" action="analysis.php" method="get">
+                    	<input type="text" style="width:400px; height: 40px; font-size:18px; padding:5px" placeholder="EnterName" name="query" /><br>
+                        <input id="sub" type="submit" class="btn btn-primary" value="submit" />
+                    </form>
+                </div>           
+            </div>
+        </div>
+    </div>
+    
+    <?php include "includes/footer.php" ?>
 </body>
 </html>
